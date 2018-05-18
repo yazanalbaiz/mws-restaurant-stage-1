57041e702bf3eef83bebfd7408b418ce040d63b9
Yazan AL-Baiz <yazobeats@gmail.com>
2018-05-18 18:57:59 +0300

    fixed: Caching should work for all pages
    Works for all pages but its really slow

--- sw.js
+++ sw.js
@@ -1,18 +1,31 @@
-let cacheName = 'RestaurantGuide v1.0';
 
+//Cache name in case we implement versioning
+let cacheName = 'RestaurantGuide v1.0';
+//Files to be cached
 let toBeCached = [
     './',
     './index.html',
-    'restaurant.html',
+    './restaurant.html',
     './css/styles.css',
     './data/restaurants.json',
     './img',
     './js/dbhelper.js',
     './js/main.js',
-    './js/restaurant_info.js'
+    './js/restaurant_info.js',
+    './img/1.jpg',
+    './img/2.jpg',
+    './img/3.jpg',
+    './img/4.jpg',
+    './img/5.jpg',
+    './img/6.jpg',
+    './img/7.jpg',
+    './img/8.jpg',
+    './img/9.jpg',
+    './img/10.jpg'
     //I couldn't for the life of me cache the maps
 ];
 
+//Add the files when sw is installed
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(cacheName)
@@ -20,6 +33,7 @@ self.addEventListener('install', event => {
     );
 });
 
+//When sw is activated, check and delete previous caches
 self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys()
@@ -33,9 +47,27 @@ self.addEventListener('activate', event => {
     );
 });
 
+//Fetch the cache
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-            .then(res => res || fetch(event.request))
+            .then(res => {
+                if (res) return res;
+                //Clone the cache so it becomes stable
+                const reqClone = event.request.clone();
+
+                fetch(reqClone)
+                    .then(res => {
+                        if(!res) return res;
+                        //Clone the response cache to pass to the promise
+                        const resClone = res.clone();
+
+                        caches.open(cacheName)
+                            .then(cache => {
+                                cache.put(event.request, resClone);
+                                return res;
+                            })
+                    })
+            })
     )
 });
